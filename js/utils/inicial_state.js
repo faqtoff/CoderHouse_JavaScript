1766f03fee7690d86bdb9815777f0627b959eb0b
Facundo Toffolo Pasquini <f.toff.pas@gmail.com>
2022-03-11 20:32:32 -0300

    Final

--- js/utils/inicial_state.js
+++ js/utils/inicial_state.js
@@ -6,6 +6,15 @@ const initalProducts = [
     {titulo:'Lays', precio: 100, stock: 3, id: 2},
     {titulo:'Chocolate', precio: 50, stock: 20, id: 3}
 ]
+/* Dolar */
+fetch('https://api.bluelytics.com.ar/v2/latest')
+.then(function(response) {
+  return response.json();
+})
+.then(function(json) {
+    console.log(json.oficial.value_sell)
+    localStorage.setItem('dolar', JSON.stringify(json))
+});
 
 /* Stock Local */
 const localProducts = JSON.parse(localStorage.getItem('products'))
